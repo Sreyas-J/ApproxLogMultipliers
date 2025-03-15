baa6fb3af73451dec613221bb90e76678779b4b1
Ajitesh Kumar Singh <Ajitesh.Kumar@iiitb.ac.in>
2025-03-15 09:20:49 +0530

    Removing Unecessary Comments

--- ArrayMuliplier/Signed_Verilog/fa.v
+++ ArrayMuliplier/Signed_Verilog/fa.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    14:18:02 05/06/2018 
-// Design Name: 
-// Module Name:    FA 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 
 module fa(input a, input b, input cin, output sum, output cout);
     wire s1, c1, c2;
